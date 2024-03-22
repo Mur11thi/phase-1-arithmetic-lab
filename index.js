994924d759f51766f05e859cdba0f1957fa24ed2
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-03-22 12:45:44 +0300

    submits arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply =( num1 * num2)
+console.log (multiply)
+
+let random = 15;
+console.log (random)
+
+let num3 = 16
+let num4 = 4
+let mod = (num3 / num4 )
+console.log ( mod)
+
+let num5 =15
+let num6 = 11
+let max = 20
+
+if(max> num6 && max > num5) {
+    console.log (max)
+}
+
+        
+    
